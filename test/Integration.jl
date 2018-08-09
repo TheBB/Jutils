534f1a03b95ca060f4b7d11bd107ca8d88435d41
Eivind Fonn <evfonn@gmail.com>
2018-08-09 11:41:33 +0200

    Integration test for Laplacian matrix

--- test/Integration.jl
+++ test/Integration.jl
@@ -28,3 +28,18 @@
         [2, 1, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1, 2] / 6,
     )
 end
+
+@testset "1D-Lagrange-Lapl" begin
+    domain, _ = line(5)
+    pfunc = outer(grad(mkbasis(domain, Lagrange, 1), 1)[:,1])
+
+    func = compile(optimize(pfunc); dense=false)
+    mass = integrate(func, domain, 1)
+    @test isa(mass, SparseMatrixCSC)
+    # @test nnz(mass) == 16
+    @test mass ≈ sparse(
+        [1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6],
+        [1, 2, 1, 2, 3, 2, 3, 4, 3, 4, 5, 4, 5, 6, 5, 6],
+        [1, -1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 1],
+    )
+end
