c4e04bff643905f1f2766ded7d393e3782dc2eb4
Eivind Fonn <evfonn@gmail.com>
2018-08-18 21:58:02 +0200

    New rectilinear constructor

--- src/Mesh.jl
+++ src/Mesh.jl
@@ -3,7 +3,7 @@ module Mesh
 using ..Functions
 using ..Topologies
 
-export line
+export line, rectilinear
 
 line(nelems::Int) = (StructuredTopology(nelems), rootcoords(1))
 
@@ -12,6 +12,10 @@ function line(nodes::StepRangeLen{T}) where T <: Real
     nelems = length(nodes) - 1
     scale = (nodes[end] - offset) / nelems
     (StructuredTopology(nelems), rootcoords(1) * scale + offset)
+end
+
+function rectilinear(nelems::Vararg{Int,N}) where N
+    (StructuredTopology(nelems...), rootcoords(N))
 end
 
 end # module
