7931bcf60b95ab81135ea8f497aed1925bb0bccd
Eivind Fonn <evfonn@gmail.com>
2018-08-19 11:25:41 +0200

    Add a 2D Lagrangian mass matrix test

--- test/Integration.jl
+++ test/Integration.jl
@@ -72,3 +72,21 @@ end
         [1, -1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 2, -1, -1, 1] * 4,
     )
 end
+
+@testset "2D-Lagrange-Mass" begin
+    domain, _ = rectilinear(2, 2)
+    pfunc = outer(mkbasis(domain, Lagrange, 1))
+
+    func = compile(pfunc; dense=false)
+    mass = integrate(func, domain, 3)
+    @test isa(mass, SparseMatrixCSC)
+    @test nnz(mass) == 49
+    @test Matrix(mass) ≈ sparse(
+        [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+         6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9],
+        [1, 2, 4, 5, 1, 2, 3, 4, 5, 6, 2, 3, 5, 6, 1, 2, 4, 5, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+         2, 3, 5, 6, 8, 9, 4, 5, 7, 8, 4, 5, 6, 7, 8, 9, 5, 6, 8, 9],
+        [4, 2, 2, 1, 2, 8, 2, 1, 4, 1, 2, 4, 1, 2, 2, 1, 8, 4, 2, 1, 1, 4, 1, 4, 16, 4, 1, 4, 1,
+         1, 2, 4, 8, 1, 2, 2, 1, 4, 2, 1, 4, 1, 2, 8, 2, 1, 2, 2, 4] / 36,
+    )
+end
