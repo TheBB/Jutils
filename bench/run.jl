d38042bccd486d410c041c42e450a76481a3776e
Eivind Fonn <evfonn@gmail.com>
2018-08-20 17:11:59 +0200

    Remove dense benchmarks

--- bench/run.jl
+++ bench/run.jl
@@ -16,18 +16,11 @@ function bench_1d_lag_mass!(suite)
     suite["lpe"]["1d"] = @benchmarkable collect($domain)
 
     mass = outer(mkbasis(domain, Lagrange, 1))
+    suite["cmp"][key] = @benchmarkable compile($mass; dense=false)
 
-    suite["cmp"][(key..., "den")] = @benchmarkable compile($mass; dense=true)
-    suite["cmp"][(key..., "spr")] = @benchmarkable compile($mass; dense=false)
-
-    dmass = compile(mass; dense=true)
-    smass = compile(mass; dense=false)
-
-    suite["mke"][(key..., "den")] = @benchmarkable callable($dmass)
-    suite["mke"][(key..., "spr")] = @benchmarkable callable($smass)
-
-    suite["itg"][(key..., "den")] = @benchmarkable integrate($dmass, $domain, 5)
-    suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
+    mass = compile(mass; dense=false)
+    suite["mke"][key] = @benchmarkable callable($mass)
+    suite["itg"][key] = @benchmarkable integrate($mass, $domain, 5)
 end
 
 function bench_1d_lag_lapl!(suite)
@@ -35,13 +28,11 @@ function bench_1d_lag_lapl!(suite)
     domain, geom = line(1000)
 
     mass = outer(grad(mkbasis(domain, Lagrange, 1), geom)[:,1])
+    suite["cmp"][key] = @benchmarkable compile($mass; dense=false)
 
-    suite["cmp"][(key..., "spr")] = @benchmarkable compile($mass; dense=false)
-
-    smass = compile(mass; dense=false)
-
-    suite["mke"][(key..., "spr")] = @benchmarkable callable($smass)
-    suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
+    mass = compile(mass; dense=false)
+    suite["mke"][key] = @benchmarkable callable($mass)
+    suite["itg"][key] = @benchmarkable integrate($mass, $domain, 5)
 end
 
 function bench_2d_lag_mass!(suite)
@@ -51,18 +42,11 @@ function bench_2d_lag_mass!(suite)
     suite["lpe"]["2d"] = @benchmarkable collect($domain)
 
     mass = outer(mkbasis(domain, Lagrange, 1))
+    suite["cmp"][key] = @benchmarkable compile($mass; dense=false)
 
-    suite["cmp"][(key..., "den")] = @benchmarkable compile($mass; dense=true)
-    suite["cmp"][(key..., "spr")] = @benchmarkable compile($mass; dense=false)
-
-    dmass = compile(mass; dense=true)
-    smass = compile(mass; dense=false)
-
-    suite["mke"][(key..., "den")] = @benchmarkable callable($dmass)
-    suite["mke"][(key..., "spr")] = @benchmarkable callable($smass)
-
-    suite["itg"][(key..., "den")] = @benchmarkable integrate($dmass, $domain, 5)
-    suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
+    mass = compile(mass; dense=false)
+    suite["mke"][key] = @benchmarkable callable($mass)
+    suite["itg"][key] = @benchmarkable integrate($mass, $domain, 5)
 end
 
 function bench_2d_lag_lapl!(suite)
@@ -71,13 +55,11 @@ function bench_2d_lag_lapl!(suite)
 
     mass = outer(grad(mkbasis(domain, Lagrange, 1), geom))
     mass = dropdims(sum(mass, (3,)), (3,))
+    suite["cmp"][key] = @benchmarkable compile($mass; dense=false)
 
-    suite["cmp"][(key..., "spr")] = @benchmarkable compile($mass; dense=false)
-
-    smass = compile(mass; dense=false)
-
-    suite["mke"][(key..., "spr")] = @benchmarkable callable($smass)
-    suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
+    mass = compile(mass; dense=false)
+    suite["mke"][key] = @benchmarkable callable($mass)
+    suite["itg"][key] = @benchmarkable integrate($mass, $domain, 5)
 end
 
 function runbench()
