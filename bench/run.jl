ceec0ca5d344a73c5698b61a941e640b8dd1ceb0
Eivind Fonn <evfonn@gmail.com>
2018-08-20 17:03:37 +0200

    Add some 2D benchmarks

--- bench/run.jl
+++ bench/run.jl
@@ -44,6 +44,42 @@ function bench_1d_lag_lapl!(suite)
     suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
 end
 
+function bench_2d_lag_mass!(suite)
+    key = ("2d", "lag", "mass")
+    domain, geom = rectilinear(30, 30)
+
+    suite["lpe"]["2d"] = @benchmarkable collect($domain)
+
+    mass = outer(mkbasis(domain, Lagrange, 1))
+
+    suite["cmp"][(key..., "den")] = @benchmarkable compile($mass; dense=true)
+    suite["cmp"][(key..., "spr")] = @benchmarkable compile($mass; dense=false)
+
+    dmass = compile(mass; dense=true)
+    smass = compile(mass; dense=false)
+
+    suite["mke"][(key..., "den")] = @benchmarkable callable($dmass)
+    suite["mke"][(key..., "spr")] = @benchmarkable callable($smass)
+
+    suite["itg"][(key..., "den")] = @benchmarkable integrate($dmass, $domain, 5)
+    suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
+end
+
+function bench_2d_lag_lapl!(suite)
+    key = ("2d", "lag", "lapl")
+    domain, geom = rectilinear(30, 30)
+
+    mass = outer(grad(mkbasis(domain, Lagrange, 1), geom))
+    mass = dropdims(sum(mass, (3,)), (3,))
+
+    suite["cmp"][(key..., "spr")] = @benchmarkable compile($mass; dense=false)
+
+    smass = compile(mass; dense=false)
+
+    suite["mke"][(key..., "spr")] = @benchmarkable callable($smass)
+    suite["itg"][(key..., "spr")] = @benchmarkable integrate($smass, $domain, 5)
+end
+
 function runbench()
     suite = BenchmarkGroup()
     suite["cmp"] = BenchmarkGroup()
@@ -54,6 +90,8 @@ function runbench()
     bench_overhead!(suite)
     bench_1d_lag_mass!(suite)
     bench_1d_lag_lapl!(suite)
+    bench_2d_lag_mass!(suite)
+    bench_2d_lag_lapl!(suite)
 
     tune!(suite)
     return run(suite)
